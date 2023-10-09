f3f2b4cff08b5fc124e16404c1c31eec1b3a10f3
rassu <senyopan@hotmail.com>
2023-10-09 23:01:54 +0900

    課題11クリア

--- src/BreedsSelect.jsx
+++ src/BreedsSelect.jsx
@@ -3,16 +3,20 @@
 import React, { useState, useEffect } from 'react';
 import DogListContainer from './DogListContainer';
 
-export const BreedsSelect = ({breeds,onChangeBreed}) => {
-
-  console.log(breeds);
-
+export const BreedsSelect = ({breeds,onChangeBreed,displayButton,urlList}) => {
+  
   return (
-    <select name="breedsList" defaultValue={breeds} onChange={onChangeBreed}>
-      {Object.values(breeds).map((breed) => (
-      <option value="item" key={breed}>{breed}</option>
+    <div>
+    <select name="breedsList" defaultValue={breeds[0]} onChange={onChangeBreed}>
+      {Object.values(breeds).map((breed,index) => (
+      <option value={breed} key={index}>{breed}</option>
       ))}
     </select>
+    <button onClick={displayButton}>表示</button>
+    {Object.values(urlList).map((url,index) => (
+    <img src={url} key={index}></img>
+      ))}
+    </div>
   )
 }
 
--- src/DogListContainer.jsx
+++ src/DogListContainer.jsx
@@ -6,13 +6,15 @@ import BreedsSelect from './BreedsSelect';
 export const DogListContainer = () => {
   const [breeds,setBreeds] = useState('');
   const [selectedBreed,setSelectedBreed] = useState('');
+  const [selectedBreedsList,setSelectedBreedsList] = useState('');
 
   useEffect(() => {
     fetch('https://dog.ceo/api/breeds/list/all')
       .then((response) => response.json())
       .then((data) => {
         const breedsList = data.message
-        setBreeds(Object.keys(breedsList));
+        setBreeds(Object.keys(breedsList))
+        setSelectedBreed(Object.keys(breedsList)[0]);
       });
       console.log(breeds)
   },[]);
@@ -21,8 +23,24 @@ export const DogListContainer = () => {
     setSelectedBreed(e.target.value);
   }
 
+  const pushDisplayButton = () => {
+    console.log('https://dog.ceo/api/breed/' + selectedBreed + '/images')
+    fetch('https://dog.ceo/api/breed/' + selectedBreed + '/images')
+    .then((response) => response.json())
+    .then((data) => {
+      const selectedList = data.message
+      console.log(selectedList)
+      const filterResult = selectedList.filter(function(value,index) {
+        return index < 12;
+      })
+      console.log(filterResult)
+      setSelectedBreedsList(filterResult);
+      console.log(selectedBreedsList)
+    });
+  }
+
   return (
-    <BreedsSelect breeds={breeds} onChangeBreed = {onChange}/>
+    <BreedsSelect breeds={breeds} onChangeBreed = {onChange} displayButton = {pushDisplayButton} urlList = {selectedBreedsList}/>
   )
 }
 
